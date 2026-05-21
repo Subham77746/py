75da9f5146b999391098cb97222b94a29b2cc2bc
Subham <sachidanandapanigrahy374@gmail.com>
2026-05-21 21:17:54 +0530

    Update 2-data types & variable.py

--- 2-data types & variable.py
+++ 2-data types & variable.py
@@ -10,23 +10,41 @@ b = 2
 print(a+b)
 
 #Data Types...
+#Funamental Data Type...
 a=5
-print(type(a)); #int
+print(type(a))   #int
 b="subham"
-print(type(b)); #string
+print(type(b))   #string
 c=2.24
-print(type(c)); #float
+print(type(c))   #float
 d=True
-print(type(d)); #boolean
+print(type(d))   #boolean
+e=10+0j
+print(type(e))   #complex
 
-list=[1,2,3,4,5,{"subham","sachi"}]
-print(type(list));  #list
+#Other Data Types...
+#1. LIST
+l1=[]                               #empty list
+l2=[1,2,3,4,5]                      #same data type list
+l3=[1,2,3,4,5,"subham","sachi"]     #diff D.T list
+print(type(list));                  #list
 
+#2.TUPLE
 f=(1,2,3,4,5)
-print(type(f)); #tuple
+print(type(f));                      #tuple
 
-#COMPLEX (real + imaginary)
+#3.COMPLEX (real + imaginary)
 g= complex(1, 8)
-print(g)        #(1+8j)
-print(type(g)); #complex
+print(g)                                #(1+8j)
+print(type(g));                         #complex
+
+#4.BYTES...
+a=[1,2,3,4,5]
+b=bytes(a)
+print(type(b))      # bytes(values of byted can't changeble)
+
+#5.BYTEARRAY...
+a=[1,2,3,4,5]
+b=bytearray(a)
+print(type(b))      # bytearray(values of bytearray can changeble)
 
